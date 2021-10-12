8af9e93daa3b0307d5f8c8ad6910b2327177b4c1
saivarapraveen <91560050+saivarapraveen@users.noreply.github.com>
2021-10-12 20:38:46 +0530

    Add files via upload
    first checkin

---
+++ camelcase_test.py
@@ -0,0 +1,4 @@
+from camelcase import CamelCase
+c = CamelCase()
+s = 'all letter in test input are lowercase. using hump method in camelcase library to covert this text into camelcase.'
+print(c.hump(s))
